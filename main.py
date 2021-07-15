c61521afe1d0d31ab3d1e6981114437e2da4c415
MCSuda <suder.michal97@gmail.com>
2021-07-15 11:46:47 +0200

    Version 3, working!

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 
 
-def genNum():
+def genNum():   ## Generate random 4 digit number
     g = ""
     i = 4
     while i > 0:
@@ -10,10 +10,9 @@ def genNum():
     return g
 
 
-def checkcow(gennumber, usernumber):
+def checkcow(gennumber, usernumber):    ## check for cows (same element with same indexes)
     cow = 0
     i = 0
-    print(gennumber)
     print(usernumber)
     for _ in gennumber:
         if gennumber[i] == usernumber[i]:
@@ -22,28 +21,32 @@ def checkcow(gennumber, usernumber):
     return cow
 
 
-def checkbull(gennumber, usernumber):
-    bull = 0
-    i = 0
-    for _ in gennumber:
-        j = 0
-        for _ in usernumber:
-            if gennumber[i] == usernumber[j]:
-                bull += 1
-            j += 1
-        i += 1
+def checkbull(gennumber, usernumber):  ## check for bulls (counting duplicates between 2 sets).
+    bull = 0                           ## it's going to count any duplicate so later we have to substract
+    i = 0                              ## cows from the bulls
+    for x in gennumber:
+        if x in usernumber:
+            bull += 1
     return bull
 
 
-y = genNum()
-print(y)
-print("Welcome to cows and bulls game!")
+if __name__ == "__main__":
+    y = genNum()
+    ## print(y)
+    print("Welcome to cows and bulls game!")
 
-while True:
-    x = input("Give me 4 digit number: ")
-    cow = checkcow(y, x)
-    bull = checkbull(y, x)
-    print("Cows: " + str(cow) + " Bulls: " + str(bull))
-    if cow == 4:
-        print("You WIN!")
-        break
+    while True:
+        x = input("Give me 4 digit number: ")
+        if len(x) < 4:      ## Just checking if given number is a 4 digit number
+            print("WRONG INPUT!")
+        elif len(x) > 4:
+            print("WRONG INPUT!")
+        else:
+            cow = checkcow(y, x)
+            bull = checkbull(set(y), set(x)) - cow  ## Checking the real number of bulls
+            if bull < 0:
+                bull = 0
+            print("Cows: " + str(cow) + " Bulls: " + str(bull))
+            if cow == 4:
+                print("You WIN!")
+                break
